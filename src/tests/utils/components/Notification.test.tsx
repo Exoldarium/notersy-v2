c0839095bfffee79e765807cd6151a0428ad1d9f
exoldarium <shandoo91@gmail.com>
2023-11-08 21:51:16 +0700

    notification message test refactoring

--- src/tests/utils/components/Notification.test.tsx
+++ src/tests/utils/components/Notification.test.tsx
@@ -1,19 +1,23 @@
 import { describe, expect, it } from "vitest";
 import { store } from "../../../store";
-import { setNotificationMessage } from "../../../reducers/messageReducer";
+import { setMessage } from "../../../reducers/messageReducer";
 import { renderWithProviders } from "../../test-utils";
 import { Notification } from "../../../components/Notification";
-import { screen } from '@testing-library/react';
+import { act } from "react-dom/test-utils";
 
 describe('<Notification />', () => {
   it('renders correctly', () => {
-    store.dispatch(setNotificationMessage({
-      type: "ERROR",
-      content: 'test content'
-    }, 5));
+    const redux = renderWithProviders(<Notification />, { store });
 
-    renderWithProviders(<Notification />, { store });
+    act(() => {
+      redux.store.dispatch(setMessage({
+        type: "ERROR",
+        content: 'test error'
+      }));
+    });
 
-    expect(screen.getByText('test content')).toBeInTheDocument();
+    const { container } = renderWithProviders(<Notification />, { store });
+
+    expect(container).toHaveTextContent('test error');
   });
 });
